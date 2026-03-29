50a8ec3a409932fb0f00b039dbf49ae62a0a9425
Ghofrane <ghofranezemni03@gmail.com>
2026-03-30 00:02:03 +0300

    fix: test

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-copier '.env.example' en '.env' et remplir par les valeurs correctes % sécuriser les données sensibles
-docker compose up --build % lancer les containers Docker
-docker ps % vois la liste des containers en marche
-docker stop <<id container>> % arreter un container
-frontend sur localhost:3000 % affichage du frontend
-backend sur localhost:8000 % affichage du backend
+git add .
+git commit -m "fix:test"
+git push origin master
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,19 +3,19 @@ from main import app
 
 client = TestClient(app)
 
-# ✅ Test 1 : Route principale
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Backend is working!"}
 
-# ✅ Test 2 : Route health check
+
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
-# ✅ Test 3 : Route inexistante
+
 def test_not_found():
     response = client.get("/route-inexistante")
     assert response.status_code == 404
